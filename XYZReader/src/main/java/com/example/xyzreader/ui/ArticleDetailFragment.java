0ecc7638d168bbbeae4b3f7b7ad2f70f287ed448
Ankur Aggarwal <roc.windwarrior@gmail.com>
2016-08-28 23:12:06 +0530

    Add the snackbar

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleDetailFragment.java
@@ -12,6 +12,7 @@ import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
@@ -192,6 +193,7 @@ public class ArticleDetailFragment extends Fragment implements
 
         mCursor = cursor;
         if (mCursor != null && !mCursor.moveToFirst()) {
+            Snackbar.make(mRootView, getResources().getString(R.string.load_error), Snackbar.LENGTH_LONG).show();
             Log.e(TAG, "Error reading item detail cursor");
             mCursor.close();
             mCursor = null;
